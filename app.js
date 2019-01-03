af7593668635b6f103f93d3e21fa49f9329970df
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-01-03 18:51:05 +0530

    using webpack-cli snd configuring config file

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-require('!style-loader!css-loader!./assets/css/style.css');
-alert('the print function is executed successfully....');
+require('./assets/css/style.css');
+alert('the print function is executed successfully!....');
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports={
+    entry:'./app.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+              test: /\.css$/,
+              use: [
+                { loader: 'style-loader' },
+                {
+                  loader: 'css-loader',
+                 
+                },
+               
+              ]
+            }
+          ]
+    }
+}
