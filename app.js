7b4eae73d5129745cb438e4c5e76ba28714127a4
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-01-20 15:17:27 +0530

    tree shaking

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('./assets/css/style.css');
+import {foo} from './assets/js/file1';
 let doSomething=require('./assets/js/dummy');
 alert('the print function is executed successfully!!!....');
-
+console.log(foo());
 if(module.hot){
 console.log('updating changes...');
 module.hot.accept('./assets/js/dummy.js',()=>{
